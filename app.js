ae88c1e7f2d4cb57342c8f6000d2eb4d731b2c44
Oscar00v <Oscar00v@gmail.com>
2025-03-26 19:44:21 -0600

    server up

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import dotenv from "dotenv";
 
-function app() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+dotenv.config({
+    path: `.env.${process.env.NODE_ENV}`
+})
 
-export default app
+import express from 'express';
+
+const app = express()
+
+// Configuracion basica de rutas HEALTCHECK
+app.get('/healtcheck', (req, res)=> {
+    const healtcheck = {
+        status: "OK",
+        code: "200",
+        message: "Servidor en funcionamiento"
+    }
+    res.send(healtcheck)
+})
+
+const PORT = 5010;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
