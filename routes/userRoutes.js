92000ae7e4468b97b63b3291200956cfb3b47c22
Oscar00v <Oscar00v@gmail.com>
2025-04-07 17:58:08 -0600

    post validation working

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,7 @@ import { createPost } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 import { get } from 'mongoose';
 import { userValidation } from '../middleware/userValidations.js';
+import { postValidation } from '../middleware/postValidationMiddleware.js';
 const router = express.Router();
 
 
@@ -18,7 +19,7 @@ router.route('/users/:email').delete(deleteUser);
 router.route('/users/update/:email').patch(updateUser);
 
 router.route('/posts').get(getPost);
-router.route('/posts/create').post(createPost);
+router.route('/posts/create').post(postValidation,createPost);
 
 router.route('/posts/protect')
   .post(protect, createPost) 
